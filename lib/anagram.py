89fa64100d8727632c8e96715d47f1b0f4ba6d50
Kevin Kelley <kkelley622@gmail.com>
2024-02-01 16:06:46 -0600

    All tests are passing.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        matching = []
+
+        for x in list:
+            if(sorted([letter for letter in x]) == sorted([letter for letter in self.word])):
+                matching.append(x)
+                
+        
+        return matching
